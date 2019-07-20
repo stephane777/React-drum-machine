c5cc5c12d7686cbb7207f9885e36c19cee8bc93a
Stephane Candelas <stephanecandelas@gmail.com>
2019-07-20 22:57:47 +0100

    webpack config

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render(){
+        return (
+            <div>
+                <h3>React Drum Machine a freecodecamp project</h3>
+            </div>
+        )
+    }
+}
